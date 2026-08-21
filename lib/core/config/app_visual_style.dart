fc66ba84c70618aac70812f4f76d26bb8028d7bd
klockky <anikitin2000@inbox.ru>
2026-08-21 21:53:52 +0300

    жижа пошел нахуй

--- lib/core/config/app_visual_style.dart
+++ lib/core/config/app_visual_style.dart
@@ -13,7 +13,7 @@ class AppVisualStyle {
 
   static final _setting = PersistedEnum<VisualStyle>(
     prefKey: prefKey,
-    defaultValue: VisualStyle.glossy,
+    defaultValue: VisualStyle.materialYou,
     encode: _encode,
     decode: _parse,
   );
@@ -27,5 +27,5 @@ class AppVisualStyle {
   static String _encode(VisualStyle value) => value.name;
 
   static VisualStyle _parse(String? val) =>
-      enumFromName(VisualStyle.values, val, VisualStyle.glossy);
+      enumFromName(VisualStyle.values, val, VisualStyle.materialYou);
 }
